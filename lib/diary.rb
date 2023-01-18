742430a7035d4d9ec6ba994ee35654d6ddcaae97
terryhycheng <terryhycheng@gmail.com>
2023-01-18 00:00:32 +0000

    Updated .make_snippet

--- lib/diary.rb
+++ lib/diary.rb
@@ -1,5 +1,10 @@
 class Diary
   def make_snippet(text)
-    text.split(" ").slice(0, 5).join(" ") + "..."
+    text_arr = text.split(" ")
+    if text_arr.length > 5
+      text_arr.slice(0, 5).join(" ") + "..."
+    else
+      text
+    end
   end
 end
--- spec/diary_spec.rb
+++ spec/diary_spec.rb
@@ -6,8 +6,13 @@ RSpec.describe Diary do
   end
 
   context ".make_snippet" do
-    it "should return the first five words and then a '...'" do
+    it "should return the first 5 words and then a '...' if the input.length > 5" do
       expect(@diary.make_snippet("This is a really simple test.")).to eq "This is a really simple..."
     end
+
+    it "should return the string itself without modification if the input.length <= 5" do
+      expect(@diary.make_snippet("This is a really test.")).to eq "This is a really test."
+    end
   end
+
 end
